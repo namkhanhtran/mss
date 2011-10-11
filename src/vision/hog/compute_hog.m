8f84c4b200ff6944cbad256e7e3bec7d6f0d66dc
Elia Bruni <elia.bruni@gmail.com>
2011-10-12 00:20:10 +0200

    hog refactoring

--- src/vision/hog/compute_hog.m
+++ src/vision/hog/compute_hog.m
@@ -1,12 +1,12 @@
-function compute_hog
+function singleDescrs =  compute_hog_test
 
 
-conf.calDir = '/Users/eliabruni/data/esp/test/input' ;
+conf.calDir = '/Users/eliabruni/data/esp/test/input/esp_sample' ;
 conf.dataDir = '/Users/eliabruni/data/esp/test/ouput/hog' ;
 conf.autoDownloadData = false ;
-conf.numTrain = 10 ;
+conf.numTrain = 100 ;
 conf.numClasses = 1 ;
-conf.numWords = 10 ;
+conf.numWords = 100 ;
 conf.numSpatialX = 1 ;
 conf.numSpatialY = 1 ;
 conf.quantizer = 'kdtree' ;
@@ -34,10 +34,10 @@ classes = {classes.name} ;
 images = {} ;
 imageClass = {} ;
 for ci = 1:length(classes)
-  ims = dir(fullfile(conf.calDir, classes{ci}, '*.jpg'))' ; 
-  ims = cellfun(@(x)fullfile(classes{ci},x),{ims.name},'UniformOutput',false) ;
-  images = {images{:}, ims{:}} ;
-  imageClass{end+1} = ci * ones(1,length(ims)) ;
+    ims = dir(fullfile(conf.calDir, classes{ci}, '*.jpg'))' ;
+    ims = cellfun(@(x)fullfile(classes{ci},x),{ims.name},'UniformOutput',false) ;
+    images = {images{:}, ims{:}} ;
+    imageClass{end+1} = ci * ones(1,length(ims)) ;
 end
 imageClass = cat(2, imageClass{:}) ;
 model.classes = classes ;
@@ -53,58 +53,59 @@ model.vocab = [] ;
 % --------------------------------------------------------------------
 
 
-if ~exist(conf.vocabPath) 
-  
-  singleDescrs = {} ;
-  
-  for ii = 1:conf.numTrain 
-    %im = imread(fullfile(conf.calDir, images{ii}));
-    %im = standardizeImage(im) ;
-    descrs = anna_phog(fullfile(conf.calDir, images{ii}), 8, 360, 3, [1;22;1;30]) ;
-    singleDescrs{ii} = vl_colsubset(single(descrs), length(descrs)) ;
-  end
-  singleDescrs
-  % Quantize the descriptors to get the visual words
-  singleDescrs = vl_colsubset(cat(2, singleDescrs{:}), 10e4) ;
-  singleDescrs = single(singleDescrs) ;
-  
-  vocab = vl_kmeans(singleDescrs, conf.numWords, 'verbose','algorithm', 'lloyd') ;
-  
-  save(conf.vocabPath, 'vocab') ;
+if ~exist(conf.vocabPath)
+    
+    singleDescrs = {} ;
+    
+    for ii = 1:conf.numTrain
+        %im = imread(fullfile(conf.calDir, images{ii}));
+        %im = standardizeImage(im) ;
+        descrs = anna_phog(fullfile(conf.calDir, images{ii}), 8, 360, 3, [1;22;1;30]) ;
+        singleDescrs{ii} = vl_colsubset(single(descrs), length(descrs)) ;
+    end
+    
+    % Quantize the descriptors to get the visual words
+    singleDescrs = vl_colsubset(cat(2, singleDescrs{:}), 10e4) ;
+    singleDescrs = single(singleDescrs) ;
+    
+    singleDescrs
+
+    vocab = vl_kmeans(singleDescrs, conf.numWords, 'verbose','algorithm', 'lloyd') ;
+    
+    save(conf.vocabPath, 'vocab') ;
 else
-  load(conf.vocabPath) ;
+    load(conf.vocabPath) ;
 end
 
 model.vocab = vocab ;
-
 if strcmp(model.quantizer, 'kdtree')
-  model.kdtree = vl_kdtreebuild(vocab) ;
+    model.kdtree = vl_kdtreebuild(vocab) ;
 end
-  
+
 % --------------------------------------------------------------------
 %                                           Compute spatial histograms
 % --------------------------------------------------------------------
 
-  blockSize = 1000 ;
-  listLength = length(images)/blockSize ;
-  histsNames = [] ;
-  for jj = 1:listLength
-     histsNames{jj} = randseq(30) ;
-  end
-  histsNames = sort(histsNames) ;
+blockSize = 1000 ;
+listLength = length(images)/blockSize ;
+histsNames = [] ;
+for jj = 1:listLength
+    histsNames{jj} = randseq(30) ;
+end
+histsNames = sort(histsNames) ;
 
 
-  hists = {} ;
-  iter = 0 ;
-  for ii = 1:length(images)
+hists = {} ;
+iter = 0 ;
+for ii = 1:length(images)
     if mod(ii, 100) == 0
-      fprintf('Processing %s (%.2f %%)\n', images{ii}, 100 * ii / length(images)) ;
+        fprintf('Processing %s (%.2f %%)\n', images{ii}, 100 * ii / length(images)) ;
     end
-
+    
     %im = imread(fullfile(conf.calDir, images{ii})) ;
     hists{ii - (iter * blockSize)} = getImageDescriptor(model, fullfile(conf.calDir, images{ii}));
-
-
+    
+    
     if mod(ii, blockSize) == 0
         tmpHists = cat(2, hists{:}) ;
         tmpHists = rot90(tmpHists) ;
@@ -118,21 +119,18 @@ end
         tmpHists = {} ;
         histName = {} ;
         iter = iter + 1 ;
-
+        
     end
+    
+end
+tmpHists = cat(2, hists{:}) ;
+tmpHists = rot90(tmpHists) ;
 
-  end
-  tmpHists = cat(2, hists{:}) ;
-  tmpHists = rot90(tmpHists) ;
-
-  eval(['ZZZZZZZZZZZZZZZZZZZZ' '= tmpHists;' ]) ;
-  % hists = cat(2, hists{:}) ;
-  conf.prefix = 'ZZZZZZZZZZZZZZZZZZZZ' ;
-  conf.histPath = fullfile(conf.dataDir, [conf.prefix '.mat']) ;
-  save(conf.histPath,'ZZZZZZZZZZZZZZZZZZZZ') ;
-
-
-
+eval(['ZZZZZZZZZZZZZZZZZZZZ' '= tmpHists;' ]) ;
+% hists = cat(2, hists{:}) ;
+conf.prefix = 'ZZZZZZZZZZZZZZZZZZZZ' ;
+conf.histPath = fullfile(conf.dataDir, [conf.prefix '.mat']) ;
+save(conf.histPath,'ZZZZZZZZZZZZZZZZZZZZ') ;
 
 
 % -------------------------------------------------------------------------
@@ -158,19 +156,20 @@ descrs = anna_phog(I, 8, 360, 3, [1;22;1;30]) ;
 
 % quantize appearance
 switch model.quantizer
-  case 'vq'
-    [drop, binsa] = min(vl_alldist(model.vocab, single(descrs)), [], 1) ;
-  case 'kdtree'
-    binsa = double(vl_kdtreequery(model.kdtree, model.vocab, ...
-                                  single(descrs),...
-                                  'MaxComparisons', 15)) ;
+    case 'vq'
+        [drop, binsa] = min(vl_alldist(model.vocab, single(descrs)), [], 1) ;
+    case 'kdtree'
+        binsa = double(vl_kdtreequery(model.kdtree, model.vocab, ...
+            single(descrs),...
+            'MaxComparisons', 15)) ;
 end
 
 
 bins = sub2ind([model.numSpatialY, model.numSpatialX, numWords], ...
-               binsa) ;
+    binsa) ;
 
 
 hist = zeros(model.numSpatialY * model.numSpatialX * numWords, 1) ;
 hist = vl_binsum(hist, ones(size(bins)), bins) ;
 hist = single(hist / sum(hist)) ;
+
